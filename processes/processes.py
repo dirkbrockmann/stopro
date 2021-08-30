e65f1c8f45384ca576910714e89c2085babba562
Ben F. Maier <benjaminfrankmaier@gmail.com>
2021-08-30 08:42:51 +0200

    some changes

--- processes/processes.py
+++ processes/processes.py
@@ -119,7 +119,7 @@ def ornsteinuhlenbeck(T,dt,variability=1,timescale=1,dimension=1,samples=1,stati
     covariance = np.identity(dimension)
     S = covariance;
     target_dimension = dimension
-    
+
     if 'steps' in kwargs:    
         steps = kwargs["steps"]
         dt = T/steps;
@@ -141,55 +141,55 @@ def ornsteinuhlenbeck(T,dt,variability=1,timescale=1,dimension=1,samples=1,stati
         covariance = S @ S.T
         dimension = m
         target_dimension = n
-        
+
     sqdt = np.sqrt(dt)
     t = np.linspace(0,T,steps+1)
-    X = {}
-    
-    
-    for i in range(samples):        
-        x = np.zeros([target_dimension,steps+1])
+    X = np.zeros((samples,target_dimension,steps+1))
+
+
+    for i in range(samples):
+        x = np.zeros((target_dimension,steps+1))
         dw = S @ np.random.randn(dimension, steps+1)
-        x[:,0]=0
-        
+        x[:,0] = 0
+
         if stationary:
             x[:,0] = S @ np.random.randn(dimension)*sigma/np.sqrt(2*theta)
-        
+
         for j in range(steps):
             x[:,j+1] = x[:,j] + (-theta) * dt * x[:,j]+ sigma * sqdt * dw[:,j]
-        
-        X[i]=x
-        
+
+        X[i] = x
+
     # das hier ist eher unueblich (dict returnen) aber kann man auch machen
     return {'variability':variability,'timescale':timescale,'noise_covariance':covariance,'steps':steps,'dt':dt,'t':t,'X':X}
-    
+
 def exponential_ornsteinuhlenbeck(T,dt,mean=1,coeff_var=1,timescale=1,dimension=1,samples=1,stationary=False,**kwargs):
-  
+
     """Generates a non-negative, multivariate stochastic process X(t) that is the exponential of an Ornstein-Uhlenbeck Process Z(t) like so X(t) = A exp( B Z(t) ). The constants A, B are chosen such that the process X(t) has the specified mean (default 1) and coefficient of variation (default 1). The Ornstein Uhlenbeck process chosen here is the solution to dZ = - Z dt + sqrt(2)*dW
-    
+
     Returns realizations on the
     time interval [0,T] at increments dt.
-    
+
     Parameters:
-    
+
     - timescale: timescale of the process (correlation time)
     - dimension: the dimension of the process
     - samples: number of realizations generated
     - mean: The mean values of the process, defaults to 1
     - coeff_var: coefficient of variation, defaults to 1
     - stationary: if set to TRUE the processes are initialized so they are stationary, if FALSE all realizations start at the origin.
-    
+
     Optional parameters:
-    
-    - steps (specifies number of time steps instead of the dt increment, 
+
+    - steps (specifies number of time steps instead of the dt increment,
       in this case dt is set to dt = T / steps)
-    
+
     - covariance = COV where COV is a real, square, positive definite covariance matrix. The covariance specifies the covariance of the Wiener increments dW.
-    
-    - mixing_matrix = S where S is an NxM matrix which is used to generate N Wiener processes W by superposition of M independent Wiener processes V, so dW = S x dV. 
-    
+
+    - mixing_matrix = S where S is an NxM matrix which is used to generate N Wiener processes W by superposition of M independent Wiener processes V, so dW = S x dV.
+
     you can either provide a covariance matrix OR a mixing_matrix, but not both
-    
+
     """
     A =  mean / np.sqrt(1+coeff_var**2); B = np.sqrt(np.log(1+coeff_var**2))
 
@@ -199,9 +199,8 @@ def exponential_ornsteinuhlenbeck(T,dt,mean=1,coeff_var=1,timescale=1,dimension=
                             samples=samples,
                             stationary=stationary,**kwargs)
 
-    for i in range(len(res["X"])):
-        res["X"][i]=A*np.exp(B*res["X"][i])
-    
+    res["X"] = A*np.exp(B*res["X"])
+
     res["mean"] = mean
     res["coeff_var"] = coeff_var
 
