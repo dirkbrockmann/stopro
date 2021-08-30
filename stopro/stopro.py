586db68b87dccd6fe5d41f0c8f98e8e137260219
Dirk Brockmannn <brockmann.dirk@gmail.com>
2021-08-30 10:57:17 +0200

    alles fettisch gemacht

--- stopro/stopro.py
+++ stopro/stopro.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Contains functions simulating the stochastic processes.
+Contains functions simulating elementary stochastic processes.
 """
 
 import numpy as np
@@ -86,6 +86,8 @@ def wiener(T,dt,dimension=1,samples=1,covariance=None,mixing_matrix=None):
         'steps':steps,
         'covariance':covariance
     }
+
+# Ornstein-Uhlenbeck Process
 
 def ornsteinuhlenbeck(T,dt,variability=1,timescale=1,dimension=1,samples=1,stationary=False,covariance=None, mixing_matrix=None,sigma=None,theta=None):
     """Generates realizations of a multivariate Ornstein-Uhlenbeck process (OUP)
@@ -225,42 +227,86 @@ def ornsteinuhlenbeck(T,dt,variability=1,timescale=1,dimension=1,samples=1,stati
         'sigma':sigma,
         'noise_covariance':covariance
     }
+
+# Exponential Ornstein-Uhlenbeck Process
     
-def exponential_ornsteinuhlenbeck(T,dt,mean=1,coeff_var=1,timescale=1,dimension=1,samples=1,stationary=False,**kwargs):
-  
-    """Generates a non-negative, multivariate stochastic process X(t) that is the exponential of an Ornstein-Uhlenbeck Process Z(t) like so X(t) = A exp( B Z(t) ). The constants A, B are chosen such that the process X(t) has the specified mean (default 1) and coefficient of variation (default 1). The Ornstein Uhlenbeck process chosen here is the solution to dZ = - Z dt + sqrt(2)*dW
+def exponential_ornsteinuhlenbeck(T,dt,mean=1,coeff_var=1,timescale=1,dimension=1,samples=1,stationary=False,covariance=None, mixing_matrix=None):
+    """Generates realizations of an exponential multivariate Ornstein-Uhlenbeck process (eOUP)
+
+    The eOUP X(t) is the exponential of an ordinary OUP Z(t) like so X(t) = A exp( B Z(t) ). 
+    The constants A, B are chosen such that the process X(t) has the specified mean (default 1) 
+    and coefficient of variation (default 1). 
+   
+    The Ornstein Uhlenbeck process chosen here is the solution to dZ = - Z dt + sqrt(2)*dW
+     
+    Returns realizations (samples) on the time interval [0,T] at increments of size dt.
+    You can specify the number of realizations, the covariance in case the processes
+    is multidimensional
     
-    Returns realizations on the
-    time interval [0,T] at increments dt.
+    Parameters
+    ----------
+    T : float
+        terminal time of time interval [0,T]
+
+    dt : float 
+        time increment
+
+    mean: float
+        the mean <X(t)> of the process
     
-    Parameters:
+    coeff_var: float
+        the coefficient of variation (standard deviation divided by the mean) of the process X(t)
     
-    - timescale: timescale of the process (correlation time)
-    - dimension: the dimension of the process
-    - samples: number of realizations generated
-    - mean: The mean values of the process, defaults to 1
-    - coeff_var: coefficient of variation, defaults to 1
-    - stationary: if set to TRUE the processes are initialized so they are stationary, if FALSE all realizations start at the origin.
+    dimension : int
+        The dimension of the process
+
+    samples : int
+        The number of samples generated
+
+    stationary : bool
+        If True, a stationary OUP is computed, i.e. initial conditions are drawn from the equilibrium pdf. 
+        If false (default), initial conditions are X(0)=0 for all components of the process.
+
+    covariance: N x N matrix
+        In case of a multivariate process the covariance matrix (N is the number of components), 
+        which must be positive semidefinite. If specified overrides dimension parameter.
+
+    mixing_matrix: N x M matrix
+        This matrix, let's call it S with elements S_ij is used to generate an 
+        N-dimensional covariant Wiener processes W (with components W_i, i=1,...,N) by superposition
+        of independent components V_j of an M-dimensional Wiener process V : W_i = sum_j S_ij * V_j.
+        The covariance of W is given by S*S^T.
+        Specifying the mixing matrix overrides the covariance parameter.
     
-    Optional parameters:
-    
-    - steps (specifies number of time steps instead of the dt increment, 
-      in this case dt is set to dt = T / steps)
-    
-    - covariance = COV where COV is a real, square, positive definite covariance matrix. The covariance specifies the covariance of the Wiener increments dW.
-    
-    - mixing_matrix = S where S is an NxM matrix which is used to generate N Wiener processes W by superposition of M independent Wiener processes V, so dW = S x dV. 
-    
-    you can either provide a covariance matrix OR a mixing_matrix, but not both
-    
+        
+    Returns
+    -------
+    dict
+        {
+            'X': dictionary of the realizations so that e.g. X[i][j] is component j of realization i
+            't': array of times
+            'dt': time increment
+            'steps': steps
+            'variability' : variability of the process
+            'timescale' : timescale of the process
+            'theta' : force factor in SDE
+            'sigma' : prefactor of noise in SDE
+            'mean' : mean of the process
+            'coeff_var' : coefficient of variation of the process
+            'noise_covariance': covariance matrix of the underlying Wiener process
+        }
+            
     """
+
     A =  mean / np.sqrt(1+coeff_var**2); B = np.sqrt(np.log(1+coeff_var**2))
 
     res = ornsteinuhlenbeck(T,dt,
                             timescale=timescale,
                             dimension=dimension,
                             samples=samples,
-                            stationary=stationary,**kwargs)
+                            stationary=stationary,
+                            covariance=covariance,
+                            mixing_matrix=mixing_matrix)
 
     for i in range(len(res["X"])):
         res["X"][i]=A*np.exp(B*res["X"][i])
