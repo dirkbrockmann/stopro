b2ea81ea000d7b06472c39ebd5a87ed7d78ae806
Dirk Brockmannn <brockmann.dirk@gmail.com>
2026-01-20 12:31:07 +0100

    uv

--- src/stopro/metadata.py
+++ src/stopro/metadata.py
@@ -3,7 +3,7 @@
 Contains a bunch of information about this package.
 """
 
-__version__ = "0.1.3"
+__version__ = "0.2.0"
 
 __author__ = "Dirk Brockmann"
 __copyright__ = "Copyright 2026-, " + __author__
